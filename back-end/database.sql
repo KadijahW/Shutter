0ff943796667c0a3403788a5a64fbfef85dbe105
PeterFiorentino <peterfiorentino@pursuit.org>
2020-01-18 11:54:07 -0500

    Gave everyone profile pics

--- back-end/database.sql
+++ back-end/database.sql
@@ -37,9 +37,9 @@ CREATE TABLE comments (
 );
 
 INSERT INTO users (username, email, profileImage)
-    VALUES ('JonathanFagan', 'JFagan@project.com', ''),
-           ('BriahanaMaugé', 'BMauge@project.com', ''),
-           ('KadijahWilson', 'KWilson@project.com', ''),
+    VALUES ('JonathanFagan', 'JFagan@project.com', 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/23161732_153227468754449_5016057889425457152_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=ewXV9KsoJnsAX9jFSsw&oh=f6debec9edd595cc3668de39dfbcc9db&oe=5E9D6D06'),
+           ('BriahanaMaugé', 'BMauge@project.com', 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/77192473_512897456245737_8807788785090691072_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=fPfdm8wfsFAAX-6LyAB&oh=644393466af580517c9c897941a5644d&oe=5EBF4FCC'),
+           ('KadijahWilson', 'KWilson@project.com', 'https://scontent-lga3-1.cdninstagram.com/v/t51.2885-19/s320x320/67972965_716653765474069_1437860166283296768_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_ohc=xc6umcOzIOQAX_bBeRD&oh=1dab5612ff9b8b9694acd8a8b6b34803&oe=5EA78B15'),
            ('PeterFiorentino', 'PFiorentino@project.com', 'http://localhost:3001/images/1578534325613-DSC02759.JPG');
 
 INSERT INTO images (poster_name, image_url, caption, alt)

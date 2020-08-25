d918e404082265ded413a656efce6d106857994b
kadjiah <kadijahwilson@pursuit.org>
2020-08-25 19:57:05 -0400

    make it better

--- back-end/app.js
+++ back-end/app.js
@@ -54,7 +54,7 @@ app.post('/upload',upload.single("image"),(req,res,next) =>{
 } )
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
 
